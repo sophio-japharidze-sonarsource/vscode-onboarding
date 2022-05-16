e7a8d466005631c6494d65beb81442fd28a366e7
sojaph <jafaridze.sofo@gmail.com>
2022-05-16 09:59:50 +0200

    wip fix test run

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,12 +1,22 @@
 import * as path from 'path';
 
-import { runTests } from '@vscode/test-electron';
+import { downloadAndUnzipVSCode, runTests } from '@vscode/test-electron';
+
+const XVFB_DISPLAY = ':10';
 
 async function main() {
 	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
+		const xDisplay = process.env['DISPLAY'];
+		if (xDisplay) {
+		  console.log(`Using DISPLAY=${xDisplay}`);
+		} else {
+		  console.warn(`No DISPLAY env variable found, exporting DISPLAY=${XVFB_DISPLAY}`);
+		  process.env['DISPLAY'] = XVFB_DISPLAY;
+		}
+
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+		const vscodeVersion = '1.52.0';
+		// const vscodeExecutablePath = await downloadAndUnzipVSCode(vscodeVersion);
 
 		// The path to test runner
 		// Passed to --extensionTestsPath
