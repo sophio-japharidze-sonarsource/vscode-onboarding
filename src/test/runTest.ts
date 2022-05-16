c1a7620e3f1ea2f273f0b58d6ac252366f7a94c0
sojaph <jafaridze.sofo@gmail.com>
2022-05-16 11:33:19 +0200

    wip fix test run add log error

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,17 +2,8 @@ import * as path from 'path';
 
 import { runTests } from '@vscode/test-electron';
 
-const XVFB_DISPLAY = ':10';
-
 async function main() {
 	try {
-		const xDisplay = process.env['DISPLAY'];
-		if (xDisplay) {
-		  console.log(`Using DISPLAY=${xDisplay}`);
-		} else {
-		  console.warn(`No DISPLAY env variable found, exporting DISPLAY=${XVFB_DISPLAY}`);
-		  process.env['DISPLAY'] = XVFB_DISPLAY;
-		}
 
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
@@ -21,6 +12,7 @@ async function main() {
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
+		console.log(err);
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
