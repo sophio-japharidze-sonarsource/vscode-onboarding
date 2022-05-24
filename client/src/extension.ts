ffb1ceed8fec75359cabda06d49a9d06436705aa
sojaph <jafaridze.sofo@gmail.com>
2022-05-24 11:33:48 +0200

    add back old features

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
-// import moment = require('moment');
-// import { GreetingProvider } from './greeting';
-// import { HelloWorldPanel } from './HelloWorldPanel';
-// import { AnimalType } from './domain';
+import moment = require('moment');
+import { GreetingProvider } from './greeting';
+import { HelloWorldPanel } from './HelloWorldPanel';
+import { AnimalType } from './domain';
 
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
@@ -18,25 +18,25 @@ let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	// console.log('Congratulations, your extension "vscode-onboarding" is now active!');
+	console.log('Congratulations, your extension "vscode-onboarding" is now active!');
 
-	// let disposable = vscode.commands.registerCommand('vscode-onboarding.heyThere', (animal : AnimalType) => {
-	// 	HelloWorldPanel.createOrShow(context.extensionUri, animal);
-	// });
+	let disposable = vscode.commands.registerCommand('vscode-onboarding.heyThere', (animal : AnimalType) => {
+		HelloWorldPanel.createOrShow(context.extensionUri, animal);
+	});
 
-	// let time = vscode.commands.registerCommand('vscode-onboarding.time', () => {
-	// 	vscode.window.showWarningMessage(`It's already ${moment().format()} !`);
-	// });
+	let time = vscode.commands.registerCommand('vscode-onboarding.time', () => {
+		vscode.window.showWarningMessage(`It's already ${moment().format()} !`);
+	});
 
-	// const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
-	// 	? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-	// const greetingProvider = new GreetingProvider(rootPath, context);		
+	const greetingProvider = new GreetingProvider(rootPath, context);		
 
-	// vscode.window.registerTreeDataProvider('greetings', greetingProvider);
+	vscode.window.registerTreeDataProvider('greetings', greetingProvider);
 
-	// context.subscriptions.push(disposable);
-	// context.subscriptions.push(time);
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(time);
 	const serverModule = context.asAbsolutePath(
 		path.join('server', 'out', 'server.js')
 	);
